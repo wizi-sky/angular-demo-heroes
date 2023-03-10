74372dc78b24290ab42905abcb50e2cc17a9588d
wangzeng <2504684962@qq.com>
2023-03-10 11:40:28 +0800

    构建表单驱动模板
    Change-Id: I63ac6420f21671d3e8b27c17dbedd298e9f0c0b8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MessageComponent } from './message/message.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   // 组件注册
@@ -24,7 +25,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HeroDetailComponent,
     MessageComponent,
     DashboardComponent,
-    HeroSearchComponent
+    HeroSearchComponent,
+    HeroFormComponent
   ],
   // angular内置模块，使用时需要手动引入
   imports: [
